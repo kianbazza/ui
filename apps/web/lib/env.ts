b9f3e4105c9d8e7d520a9ea2bf3e347fa963a091
Kian Bazza <kian@bazza.dev>
2025-03-25 17:03:47 -0400

    Fix env again

--- apps/web/lib/env.ts
+++ apps/web/lib/env.ts
@@ -8,8 +8,9 @@ export const env = createEnv({
   runtimeEnv: {
     NEXT_PUBLIC_APP_URL:
       process.env.NEXT_PUBLIC_VERCEL_ENV === 'production'
-        ? process.env.NEXT_PUBLIC_VERCEL_PROJECT_PRODUCTION_URL
-        : (process.env.NEXT_PUBLIC_VERCEL_BRANCH_URL ??
-          process.env.NEXT_PUBLIC_APP_URL),
+        ? `https://${process.env.NEXT_PUBLIC_VERCEL_PROJECT_PRODUCTION_URL}`
+        : process.env.NEXT_PUBLIC_VERCEL_BRANCH_URL
+          ? `https://${process.env.NEXT_PUBLIC_VERCEL_BRANCH_URL}`
+          : process.env.NEXT_PUBLIC_APP_URL,
   },
 })
