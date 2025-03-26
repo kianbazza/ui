bc8e365ee7568bdda91419ba3fdb9631922eb88d
Kian Bazza <kian@bazza.dev>
2025-03-26 12:51:52 -0400

    Drop onLoad logging

--- apps/web/app/stats.tsx
+++ apps/web/app/stats.tsx
@@ -1,14 +1,10 @@
-'use client'
-
 import Script from 'next/script'
-
 export default function OneDollarStatsScript() {
   return (
     <Script
       defer
       src="https://assets.onedollarstats.com/stonks.js"
       id="stonks"
-      onLoad={() => console.log('Loaded OneDollarStats script')}
       data-debug="ui.bazza.dev"
     />
   )
