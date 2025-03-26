c072f772d3d4a5216b23da7115ea3b1f1d5c2077
Kian Bazza <kian@bazza.dev>
2025-03-26 16:44:10 -0400

    Redirects

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -2,6 +2,23 @@ import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
   transpilePackages: ['next-mdx-remote'],
+  redirects: async () => [
+    {
+      source: '/docs',
+      destination: '/docs/intro',
+      permanent: false,
+    },
+    {
+      source: '/filters',
+      destination: '/docs/data-table-filter',
+      permanent: false,
+    },
+    {
+      source: '/r/filters',
+      destination: '/r/data-table-filter.json',
+      permanent: false,
+    },
+  ],
 }
 
 export default nextConfig
